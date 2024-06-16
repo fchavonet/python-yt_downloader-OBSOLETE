f51e4b10a6f633332421eb6e5116eed4fd68bd00
fchavonet <fchavonet@mailo.com>
2024-06-16 14:25:12 +0200

    Creating `CustomTkinter` application setup.

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+A simple YouTube video downloader.
+"""
+
+import customtkinter
+
+# Set the appearance mode and default color theme.
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+# Create the main application window.
+app = customtkinter.CTk()
+app.resizable(False, False)
+
+# Start the application's main loop.
+app.mainloop()
